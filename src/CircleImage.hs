c7215548bd51037b88098145617c28ff722c04a1
Matt Richard <matt@richardsoftware.com>
2016-03-20 17:56:26 -0500

    Add RVar methods for Cirlce and CircleImage

--- src/CircleImage.hs
+++ src/CircleImage.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE FlexibleContexts #-}
 module CircleImage where
 
 import Codec.Picture
 import Graphics.Rasterific
 import Graphics.Rasterific.Texture
-import qualified Data.Vector.Storable as V
+import Data.Random
+import Control.Monad
+
 
 data Circle =
   Circle { circleX :: Int
@@ -12,8 +13,69 @@ data Circle =
          , circleR :: Int
          , circleColor :: PixelRGBA8
          }
+  deriving (Show)
+
 
 type CircleImage = [Circle]
+
+
+circleImageGen :: Int -> Int -> Int -> RVar CircleImage
+circleImageGen maxCircles w h = do
+  -- n <- uniform 1 maxCircles
+  let n = maxCircles
+  replicateM n $ circleGen w h
+
+
+circleGen :: Int -> Int -> RVar Circle
+circleGen imageW imageH = do
+  r <- (+15) <$> normalInt 30
+  x <- uniform (-r) (imageW+r-1)
+  y <- uniform (-r) (imageH+r-1)
+
+  c <- uniformColor
+
+  return $ Circle x y r c
+
+  where
+    uniformColor = PixelRGBA8 <$> col <*> col <*> col <*> col
+    col = uniform 0 255
+
+
+normalInt :: Int -> RVar Int
+normalInt s = do
+  x <- stdNormal :: RVar Float
+  return (round (x * fromIntegral s))
+
+
+tweakCircleImage :: Int -> CircleImage -> RVar CircleImage
+tweakCircleImage s =
+  mapM (tweakCircle s)
+
+tweakCircle :: Int -> Circle -> RVar Circle
+tweakCircle s (Circle x y r c) = do
+  dx <- normalInt s
+  dy <- normalInt s
+  dr <- normalInt s
+
+  c' <- tweakColor c
+
+  return $ Circle (x+dx) (y+dy) (r+dr) c'
+
+  where
+    tweakColor (PixelRGBA8 r g b a) = do
+      r' <- colorDelta r
+      g' <- colorDelta g
+      b' <- colorDelta b
+      a' <- colorDelta a
+
+      return $ PixelRGBA8 r' g' b' a'
+
+    colorDelta x = do
+      dx <- normalInt 64
+      return $ clamp (fromIntegral x + dx)
+
+    clamp x = fromIntegral $ max 0 (min 255 x)
+
 
 renderCircle :: Circle -> Drawing PixelRGBA8 ()
 renderCircle (Circle x y r c) =
@@ -27,93 +89,4 @@ renderCircle (Circle x y r c) =
 renderCircles :: CircleImage -> Drawing PixelRGBA8 ()
 renderCircles = mapM_ renderCircle
 
-
-testCircles =
-  [ Circle 30 30 15 (PixelRGBA8 178 40 255 255)
-  , Circle 60 60 15 (PixelRGBA8 78 40 255 255)
-  , Circle 90 90 45 (PixelRGBA8 0 40 25 55)
-  ]
-
-translateCircle dx dy (Circle x y r c) =
-  Circle (x + dx) (y + dy) r c
-
-
-test1 =
-  renderWhite 1280 1014 $
-  renderCircles testCircles
-
-test2 =
-  renderWhite 1280 1014 $
-  renderCircles (map (translateCircle 10 10) testCircles)
-
-test3 =
-  renderWhite 1280 1014 $
-  renderCircles (map (translateCircle 10 50) testCircles)
-
-renderWhite w h drawing =
-  renderDrawing w h white drawing
-  where
-    white = PixelRGBA8 255 255 255 255
-
-
-diffImages :: Pixel a => Image a -> Image a -> Image PixelRGBA8
-diffImages img1@(Image w1 h1 _) img2@(Image w2 h2 _) =
-  generateImage diff' w h
-  where
-    w = max w1 w2
-    h = max h1 h2
-    diff' x y =
-      if p1 == p2 then green else red
-      where
-        p1 = pixelAt img1 x y
-        p2 = pixelAt img2 x y
-
-    green = PixelRGBA8   0 200   0 255
-    red   = PixelRGBA8 200   0   0 255
-
-
-fitness :: Image PixelRGBA8 -> Image PixelRGBA8 -> Double
-fitness source@(Image w h _) target =
-  raw
-  where
-    raw =
-      sum [diff (s x y) (t x y) | x <- [0..w-1]
-                                , y <- [0..h-1]]
-    s = pixelAt source
-    t = pixelAt target
-
-    (-.) cs ct =
-      delta * delta
-      where
-        delta = fromIntegral cs / 255 - fromIntegral ct / 255
-
-    diff :: PixelRGBA8 -> PixelRGBA8 -> Double
-    diff (PixelRGBA8 r g b _) (PixelRGBA8 r' g' b' _) =
-      r -. r' +
-      g -. g' +
-      b -. b'
-
-
-fitness' :: Image PixelRGBA8 -> Image PixelRGBA8 -> Double
-fitness' (Image _ _ source) (Image _ _ target) =
-  sum $ zipWith deltaSq (V.toList source) (V.toList target)
-  where
-    deltaSq a b =
-      delta * delta
-      where delta = fromIntegral a / 255 - fromIntegral b / 255
-
-
-fitness'' :: Image PixelRGBA8 -> Image PixelRGBA8 -> Double
-fitness'' (Image _ _ source) (Image _ _ target) =
-  V.ifoldl' worker 0 source
-  where
-    worker acc i x = acc + deltaSq x (target V.! i)
-    deltaSq x x' =
-      let delta = toD x - toD x'
-      in delta * delta
-    toD x = fromIntegral x / 255
-
-renderTest :: IO ()
-renderTest =
-  writePng "test.png" (diffImages test1 test2)
 
